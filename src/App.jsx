4bf73b9eec910b26b3209d273b070c072398f1ef
KhoaT <khoa.tran@eastagile.com>
2023-10-22 20:31:09 +0700

    Initialize prj

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Slider from './Slider'
+
+function App() {
+  return (
+    <>
+      <div>
+
+      </div>
+      <div className="mt-32">
+        <Slider />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Slider.jsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import "yet-another-react-lightbox/plugins/captions.css";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+// import Inline from "yet-another-react-lightbox/plugins/inline";
+
+export default function Slider() {
+const [open, setOpen] = React.useState(true);
+
+return (
+<>
+    <button type="button" onClick={() => setOpen(true)}>
+    Open Lightbox
+    </button>
+
+    <Lightbox
+    plugins={[Captions, Slideshow]}
+    open={open}
+    close={() => setOpen(false)}
+    slides={[
+        { 
+            src: "https://source.unsplash.com/ts1zXzsD7xc/1080x1620",
+            title: "Slide title",
+            description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." 
+        },
+        { 
+            title: "Slide title",
+            description: "Slide description",
+            src: "https://source.unsplash.com/F_r83HEzsXI/1080x1426" 
+        },
+        { 
+            title: "Slide title",
+            description: "Slide description",
+            src: "https://source.unsplash.com/m82uh_vamhg/1080x1440" },
+    ]}
+    captions={{ showToggle: true, descriptionTextAlign: "center", descriptionMaxLines: 5 }}
+    slideshow={{ autoplay: true, delay: 2000 }}
+    // inline={{
+    //     style: { width: "100%", maxWidth: "100vw", aspectRatio: "3 / 2" },
+    // }}
+    />
+</>
+);
+}
